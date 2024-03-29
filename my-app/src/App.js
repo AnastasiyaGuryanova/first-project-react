cd24bc2c59bf55473cf8bf632b5631262790ad62
Anastasiya Guryanova <gurianova.lan@mail.ru>
2024-03-29 08:45:58 +0300

    application without JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,39 @@
-/* декларативный  */
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+	return React.createElement('div', {
+		className: 'App',
+		children: React.createElement('header', {
+			className: 'App-header',
+			children: [
+				React.createElement('img', {
+					src: logo,
+					className: 'App-logo',
+					alt: 'logo',
+				}),
 
-				<p>{new Date().getFullYear()}</p>
-			</header>
-		</div>
-	);
+				React.createElement('p', {
+					children: [
+						'Edit ',
+						React.createElement('code', {
+							children: 'src/App.js',
+						}),
+						' and save to reload.',
+					],
+				}),
+
+				React.createElement('a', {
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+					children: 'Learn React',
+				}),
+
+				React.createElement('p', { children: new Date().getFullYear() }),
+			],
+		}),
+	});
 };
